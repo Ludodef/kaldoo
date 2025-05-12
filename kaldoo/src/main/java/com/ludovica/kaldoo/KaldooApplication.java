0ceec9348f46cd3e3f3e7747973359b30b0f6540
Ludodef <defazioludovica95@gmai.com>
2025-05-12 15:35:08 +0200

    creazione file

---
+++ kaldoo/src/main/java/com/ludovica/kaldoo/KaldooApplication.java
@@ -0,0 +1,13 @@
+package com.ludovica.kaldoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaldooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaldooApplication.class, args);
+	}
+
+}
